41bd918a988581ed73c12a7caa7578e1f7a59993
Lyle Xu <106456929+LyleXX@users.noreply.github.com>
2025-04-27 17:50:19 +0800

    feat:format error,extract configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,24 +5,40 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3307,
-      username: 'root',
-      password: '123456',
-      database: 'zeus',
-      synchronize: true,
-      logging: true,
-      entities: [User],
-      poolSize: 10,
-      connectorPackage: 'mysql2',
-      extra: {
-        authPlugin: 'sha256_password',
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'src/.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory(configService: ConfigService) {
+        console.log('Database Config:', {
+          host: configService.get('MYSQL_SERVER_HOST'),
+          port: configService.get('MYSQL_SERVER_PORT'),
+          username: configService.get('MYSQL_SERVER_USER'),
+          database: configService.get('MYSQL_SERVER_DB'),
+        });
+        return {
+          type: 'mysql',
+          host: configService.get('MYSQL_SERVER_HOST'),
+          port: configService.get('MYSQL_SERVER_PORT'),
+          username: configService.get('MYSQL_SERVER_USER'),
+          password: configService.get('MYSQL_SERVER_PASSWORD'),
+          database: configService.get('MYSQL_SERVER_DB'),
+          synchronize: true,
+          logging: true,
+          entities: [User],
+          poolSize: 10,
+          connectorPackage: 'mysql2',
+          extra: {
+            authPlugin: 'sha256_password',
+          },
+        };
       },
+      inject: [ConfigService],
     }),
     JwtModule.register({
       global: true,
